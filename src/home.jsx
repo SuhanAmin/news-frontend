39209191d32d7256aca1a720af3889852649eccd
SuhanAmin <zorofurryking@gmail.com>
2025-07-03 12:43:10 +0530

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import "./home.css"
-import { set } from 'mongoose';
+
 
  function Home() {
   const navigate=useNavigate()
